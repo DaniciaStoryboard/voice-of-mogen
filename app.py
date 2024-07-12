a83cb5cc95435ebf69c78bb2c1037c62f7b8f72d
DaniciaStoryBoard <daniv112110@gmail.com>
2024-07-12 05:36:20 -0700

    Update usage of env vars

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
 def create_app():
-
+    load_dotenv()
+    project_name = os.environ["GOOGLE_PROJECT"]
+    print(project_name)
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "*"}})
 
     @app.route("/hello", methods=["POST", "GET"])
     def hello():
-        return "hello, world"
+        return "hello, " + project_name
     
     return app
