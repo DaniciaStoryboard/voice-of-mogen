70b40c6c770957d94cbea1d88962c94d4de0528d
DaniciaStoryBoard <daniv112110@gmail.com>
2024-07-12 05:51:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 import os
 
 def create_app():
-    load_dotenv()
-    project_name = os.environ["GOOGLE_PROJECT"]
+    # load_dotenv()
+    project_name = "ai" # os.environ["GOOGLE_PROJECT"]
     print(project_name)
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "*"}})
